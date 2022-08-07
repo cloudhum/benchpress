3d49235b79a10d7ef26947ff386fd1e5c2b1c5a2
cloudhum <turbulentbees@gmail.com>
2022-08-06 18:55:14 -0700

    Device Info UI: changed Device Info header to link

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 let deviceInfoTitle = document.querySelector('.device-info-title');
 let deviceInfo = document.querySelector('.device-info');
 
-deviceInfoTitle.addEventListener('click', function(){
+function deviceInfoShowHide() {
     let currentStyle = window.getComputedStyle(deviceInfo, null).display;
     if (currentStyle == "none") {
         deviceInfo.style.display = "block";
     } else if (currentStyle == "block") {
         deviceInfo.style.display = "none";
     }
-})
-
-// TODO: make 'Device Info' a link and call hide/show function by name
+}
 
 // TODO: add span to titles of Device Info data 
