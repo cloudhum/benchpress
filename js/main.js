45ed276563e0e8113b7fc9664290480a426e8914
cloudhum <turbulentbees@gmail.com>
2022-08-06 18:35:43 -0700

    Device Info UI: add toggle visibility

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-document.querySelector('.device-info-title').addEventListener('click', function() {
-    console.log("Device Info Plexed")
+let deviceInfoTitle = document.querySelector('.device-info-title');
+let deviceInfo = document.querySelector('.device-info');
+
+deviceInfoTitle.addEventListener('click', function(){
+    let currentStyle = window.getComputedStyle(deviceInfo, null).display;
+    if (currentStyle == "none") {
+        deviceInfo.style.display = "block";
+    } else if (currentStyle == "block") {
+        deviceInfo.style.display = "none";
+    }
 })
+
+// TODO: make 'Device Info' a link and call hide/show function by name
+
+// TODO: add span to titles of Device Info data 
