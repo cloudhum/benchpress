ce2f1357a1eec3a3c8a2b47d9123dd34417dafbc
cloudhum <100453050+cloudhum@users.noreply.github.com>
2022-08-07 20:34:06 -0700

    API calls: add page load API call (#18)
    Set light box color based on API data when page loads. If the light is off, adjust rgb value to gray and cross out box.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
+let rgbValues;
+let light = document.querySelector("#light")
 let deviceInfoTitle = document.querySelector('.device-info-title');
 let deviceInfo = document.querySelector('.device-info');
+
+async function getLightStatus() {
+    const url = 'http://busyserv.mchaker.github.beta.tailscale.net:8080/light/0/status'
+    let response = await fetch(url);
+
+    let data = await response.json();
+    rgbValues = data["rgb"];
+    setLightColor(rgbValues);
+}
+
+function setLightColor(rgbValues) {
+    if (JSON.stringify(rgbValues)==JSON.stringify([0,0,0])) {
+        rgbValues = [235, 235, 235] 
+    } else {
+        light.classList.remove('light-x')
+    }
+    document.querySelector('.light').style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`
+    console.log("set light color to " + rgbValues)
+}
 
 function deviceInfoShowHide() {
     let currentStyle = window.getComputedStyle(deviceInfo, null).display;
@@ -9,6 +30,3 @@ function deviceInfoShowHide() {
         deviceInfo.style.display = "none";
     }
 }
-
-// let li = document.querySelector("li")
-// li.innerHTML += "Light 1"
